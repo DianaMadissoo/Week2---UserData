f59254ea459878323140a06000a1d0c80853469d
Diana Madissoo <dmadisso@tktk.ee>
2021-09-20 16:40:39 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace UserData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Programm küsib kasutaja sünniaastat;
+            //Programm arvutab kasutaja vanust;
+            //programm kuvab kasutaja vanust konsoolis;
+
+            Console.WriteLine("Palun, sisesta oma sünniaasta:");
+            int yearOfBirth = Convert.ToInt32(Console.ReadLine());
+
+            int userAge = 2021 - yearOfBirth;
+
+            //Console.WriteLine("Oled " + userAge + " aastat vana.");
+
+            //strig interpolation
+            Console.WriteLine($"Oled {userAge} aastat vana.");
+
+
+        }
+    }
+}
